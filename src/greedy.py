feef349907ed8376843c3f6321c1bdcf3728975d
fatimah6522 <fatimah6522@gmail.com>
2026-04-01 18:36:21 -0500

    added array to track path, returns path as well as cost

--- src/greedy.py
+++ src/greedy.py
@@ -1,24 +1,29 @@
 import numpy as np
 
 def greedy(board):
+    
     numCities = len(board)
     visited = np.zeros(numCities, dtype=int)
+    path = [0]
+    
     visited[0] = 1 #mark starting city as visited so it is ignored
+    
     currCity = 0
     cost = 0
     
     for i in range(numCities):
         minCost = 1000 #higher than max cost (100)
         
-        for j in range(numCities): #find smallest cost
+        for j in range(numCities):
             if (not visited[j]) and (board[currCity][j] < minCost):
                 minCost = board[currCity][j]
                 nextCity = j
-        
+                
+        path.append(nextCity)
         visited[nextCity] = True
         cost += board[currCity][nextCity]
         currCity = nextCity
         
     cost += board[currCity][0]
-    
-    return cost
+    path[-1] = 0
+    return cost, path
