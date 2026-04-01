dfec11a4ad374fc0f9ca7f97acc21bcf6a53854f
fatimah6522 <fatimah6522@gmail.com>
2026-04-01 18:35:29 -0500

    added path for greedy, added size parameter to generateBoard

--- src/main.py
+++ src/main.py
@@ -1,21 +1,19 @@
 import utils
 import greedy
 import genetic
-# import bfs
 # import dfs
-import aStarTsp #might need .py
+import aStarTsp
 
-board = utils.generateBoard()
+board = utils.generateBoard(10) # 10x10
 utils.printBoard(board)
 
-greedyCost=greedy.greedy(board)
+greedyCost, greedyPath = greedy.greedy(board)
 geneticPath, geneticCost = genetic.genetic(
     board,
     popSize=200,
     generations=500,
     mutationRate=0.05
 )
-# bfsCost=bfs.bfs(board)
 # dfsCost=dfs.dfs(board)
 
 print("\nCost to make all sales using:")
@@ -23,10 +21,8 @@ print(f'Greedy = {greedyCost}')
 print(f'Genetic = {geneticCost}')
 
 print("\nBest paths:")
+print(f'Greedy Path = {greedyPath}')
 print(f"Genetic Path = {geneticPath}\n")
 print(f'aStarTsp = ')
 aStarTsp.aStarTsp(board)
-# print(f'BFS = {bfsCost}')
 # print(f'DFS = {dfsCost}')
-
-#add whatever other searches u used
