9013ab66fca11ea6700a3038e5aabe7b3945e79f
fatimah6522 <fatimah6522@gmail.com>
2026-04-01 18:38:53 -0500

    generateBoard now works with different sizes, printBoard now also prints size details

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-def generateBoard():
+def generateBoard(size):
     
-    board=np.zeros((10,10), int)
+    board=np.zeros((size,size), int)
     
-    for i in range(10):
-        for j in range(10-i):
+    for i in range(size):
+        for j in range(size-i):
             board[i][j+i] = np.random.randint(1,21)*5
     
     board = board + np.transpose(board)
@@ -21,10 +21,12 @@ def generateBoard():
     each row i is a location i
     each x in column j is the cost to move from location i to location j
     
-    x is a random multiple of 5 between 5 and 100, symmetrical around the diagonal
+    x is a random multiple of 5 between 5 and 100
     '''
     
     return board
 
 def printBoard(board):
+    n = len(board)
     print(board)
+    print(f'{n}x{n} board, {n} locations')
